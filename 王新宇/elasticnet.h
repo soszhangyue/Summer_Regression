3937431d0bfbc913920af4ce06c0fd20519aeb33
Meteor-Wang <110979007+Meteor-Wang@users.noreply.github.com>
2022-08-29 11:21:54 +0800

    Add files via upload

---
+++ 王新宇/elasticnet.h
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <regex>
+
+
+
+using namespace std;
+using namespace Eigen;
+
+#define OUTPUT_PATH_X "D:\\school\\Prime\\data\\dataX_120_2.txt"
+#define OUTPUT_PATH_Y "D:\\school\\Prime\\data\\dataY_120_2.txt"
+
+typedef struct {
+    VectorXd meanPredictor;
+    VectorXd stdPredictor;
+    double meanResponse;
+    double stdResponse;
+} NormVar;
+
+typedef struct {
+    MatrixXd predictor;
+    VectorXd response;
+    NormVar normVar;
+} RegressionVar;
+
+
+//读取矩阵函数
+MatrixXd readVariable_m_x();
+
+//读取向量函数
+MatrixXd readVariable_m_y();
+
+//得到标准化所需的条件
+RegressionVar normlization(MatrixXd X, VectorXd Y);
+    /*
+    Description: Normalize predictors and responses by their mean and standard deviation.
+    Input:
+        MatrixXd X: predictors.
+        VectorXd Y: responses.
+    Output:
+        RegressionVar: normalized predictors, responses and normvar containing mean and std.
+    */
+
+    //对X的标准化
+MatrixXd normPredictor(MatrixXd X, NormVar normvar);
+
+//对Y的归一化
+VectorXd normresponse(VectorXd Y, NormVar normvar);
+
+//计算过程的内置函数
+double S(double z, double gamma);
+
+//协方差更新
+VectorXd coordinateDescentCovariance(MatrixXd X, VectorXd Y, double alpha, double lambda);
+
+//朴素更新
+VectorXd coordinateDescentNaive(MatrixXd X, VectorXd Y, double alpha, double lambda);
+
+//pathwiseLearning
+VectorXd pathwiseLearning(MatrixXd X, VectorXd Y, double alpha);
+
+//对Y的逆归一化
+VectorXd invNormResponse(VectorXd Y, NormVar normvar);
---
+++ 王新宇/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <regex>
+#include "elasticnet.h"
+
+
+using namespace std;
+using namespace Eigen;
+
+#define OUTPUT_PATH_X "D:\\school\\Prime\\data\\dataX_120_2.txt"
+#define OUTPUT_PATH_Y "D:\\school\\Prime\\data\\dataY_120_2.txt"
+
+
+int main()
+{
+    int N, P;
+    MatrixXd X;
+    VectorXd Y;
+
+    X = readVariable_m_x();
+    Y = readVariable_m_y();
+    N = X.rows();
+    P = X.cols();
+    VectorXd beta_vector = Eigen::VectorXd::Zero(P);
+
+    cout << "X是" << endl << X << endl;
+    cout << "Y是" << endl << Y << endl;
+
+    RegressionVar a;
+    a = normlization(X, Y);
+    
+    MatrixXd normX = normPredictor(X, a.normVar);
+    VectorXd normY = normresponse(Y, a.normVar);
+
+    cout << endl << "normX是" << endl << normX << endl;
+    cout << endl << "normY是" << endl << normY << endl;
+
+    cout << endl << "请选择您想要进行的算法：(a,b,c分别代表朴素，协方差以及pathwise)" << endl;
+    string method;
+    cin >> method;
+    if (method == "a")
+    {
+        cout << endl << "请输入需要的lambda，alpha:(请用回车键隔开)" << endl;
+        double lambda_input, alpha_input;
+        cin >> lambda_input;
+        cin >> alpha_input;
+        beta_vector = coordinateDescentNaive(normX, normY, alpha_input, lambda_input);
+    }
+    else if (method == "b")
+    {
+        cout << endl << "请输入需要的lambda，alpha:(请用回车键隔开)" << endl;
+        double lambda_input, alpha_input;
+        cin >> lambda_input;
+        cin >> alpha_input;
+        beta_vector = coordinateDescentCovariance(normX, normY, alpha_input, lambda_input);
+    }
+    else if (method == "c")
+    {
+        cout << endl << "请输入需要的alpha:" << endl;
+        double alpha_input;
+        cin >> alpha_input;
+        beta_vector = pathwiseLearning(normX, normY, alpha_input);
+    }
+    else
+        cout << endl << "请检查输入是否正确！" << endl;
+
+
+    cout << endl << "beta_vector是：" << endl << beta_vector<<endl;
+
+    VectorXd computY;
+    computY = normX * beta_vector;
+    VectorXd invnormY;
+    invnormY = invNormResponse(computY, a.normVar);
+    cout << endl << "逆标准的Y预测是：" << endl << invnormY<<endl;
+
+    cout << endl << "原Y是：" << endl << Y<<endl;
+
+    VectorXd err_Y_vector;
+    err_Y_vector = invnormY - Y;
+    cout << endl << "预测误差是" << endl << err_Y_vector << endl;
+
+    double total_err_Y = 0;
+    for (int i = 1; i <= N; i++)
+        total_err_Y += err_Y_vector(i - 1) * err_Y_vector(i - 1);
+    total_err_Y /= (double)N;
+    cout << endl << "误差总量计算为" << endl << total_err_Y << endl;
+
+    return 0;
+}
